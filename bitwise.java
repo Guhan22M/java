3b3cc3f60bb664720a999af73ce811772cd4b480
Guhan22M <133367863+Guhan22M@users.noreply.github.com>
2023-06-15 15:44:49 +0000

    bitwise

---
+++ bitwise.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class bitwise{
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int a=scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the second number: ");
+        int b=scanner.nextInt();
+        scanner.nextLine();
+        int c=a & b,d=a | b,e=a ^ b;
+        System.out.println("AND operator of "+a+"and "+b+"is "+c);
+        System.out.println("OR operator of "+a+"and "+b+"is "+d);
+        System.out.println("XOR operator of "+a+"and "+b+"is "+e);
+    }
+}
