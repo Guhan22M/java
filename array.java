112b13782d86c353b24b10f247011b53dfada530
Guhan22M <133367863+Guhan22M@users.noreply.github.com>
2023-06-25 14:31:31 +0000

    array

---
+++ array.java
@@ -0,0 +1,12 @@
+public class array{
+    public static void main(String args[]) {
+        int x[]=new int[50];
+        x[0]=34;
+        x[1]=98;
+        x[2]=100;
+        x[3]=65;
+        int y[]={76,84,86,35,99,23,18};
+        System.out.println(x[2]);
+        System.out.println(y[3]);
+    }
+}
